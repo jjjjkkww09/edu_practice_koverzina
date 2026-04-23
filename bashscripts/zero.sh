8da1fabbdf7c0da2d27e53e2214bbf341233f54a
kwk <0r945md0n0r@gmail.com>
2026-04-23 14:52:41 +0700

    Add files via upload

---
+++ bashscripts/zero.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+group=321
+name="Анастасия"
+surname="Коверзина"
+stipendia=778,5
+stipendiadollar=10,38
+
+echo "Я учусь в $group, зовут меня $name $surname. В этом месяце мне пришла стипендия размеров в $stipendia рублей, это 
+$stipendiadollar в \$."
